a0a4c78c784e36388a222f0322e2fd329971dbbe
Roman Avanesian <avanesianroman@gmail.com>
2026-05-12 16:19:59 +0300

    added CardOrderTest

---
+++ src/test/java/ru/netology/selenium/CardOrderTest.java
@@ -0,0 +1,54 @@
+package ru.netology.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CardOrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+
+        driver.get("http://localhost:9999/");
+    }
+
+    @AfterEach
+    public void tearDowm() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+
+    void shouldSubmitRequest() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Роман Аванесян");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+72345678901");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("[type=button]")).click();
+
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
+        assertTrue(result.isDisplayed());
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", result.getText().trim());
+    }
+}
